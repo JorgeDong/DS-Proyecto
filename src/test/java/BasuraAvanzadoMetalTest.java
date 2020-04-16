3e3f00735c4f8f438d0bfe49c08c38e6c5f9d20b
Jorge <jdongllaug@gmail.com>
2020-04-15 19:06:46 -0500

    FirstTest

--- src/test/java/BasuraAvanzadoMetalTest.java
+++ src/test/java/BasuraAvanzadoMetalTest.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dsproyectop1.ds.proyectop1.factory.niveles;
 
 /**
  *
  * @author coque
  */
-public class BasuraBasicoOrganica {
+public class BasuraAvanzadoMetalTest {
     
 }
